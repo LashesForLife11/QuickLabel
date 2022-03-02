b63a26d4e8de4af5cce8a284f4df0df1e0702d76
Michal Kurowski <michalkurowski@MBPsomtorMichal.localdomain>
2022-03-02 13:07:05 +0100

    Implement Table height

--- services/client/src/components/Table/Table.jsx
+++ services/client/src/components/Table/Table.jsx
@@ -2,29 +2,31 @@ import { Checkbox } from '@/components';
 
 export default ({ data, onChange }) => {
     const count = data.length;
-
+    console.log(data);
     return (
         <table>
             <thead>
                 <tr>
-                    <td>{count > 0 && count}</td>
-                    <td>Fullname</td>
-                    <td>Address</td>
-                    <td>City</td>
+                    <th>Fullname</th>
+                    <th>Address</th>
+                    <th>City</th>
+                    <th>Country</th>
+                    <th>{count > 0 && count}</th>
                 </tr>
             </thead>
             <tbody>
                 {data.map(item => (
                     <tr key={item.order_id}>
                         <td>
-                            <Checkbox onChange={onChange} />
-                        </td>
-                        <td>
                             {item.firstname} {item.lastname}
                         </td>
                         <td>{item.address}</td>
                         <td>
                             {item.city} {item.zipcode}
+                        </td>
+                        <td>{item.country}</td>
+                        <td>
+                            <Checkbox onChange={onChange} />
                         </td>
                     </tr>
                 ))}
